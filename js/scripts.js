0c76d11083f40bac13701eb68fdf816eec37a543
LeNapravnik <lezinapoli@gmail.com>
2022-10-14 15:57:02 +0200

    added jquery, scripts for viewing overlays with price lists

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+
+$( document ).ready(function() {
+    
+	$(".price-butt").click(function() {
+        let buttId = $(this).attr("id");
+        $("." + buttId).css("display", "block");
+	});
+
+    $(".felling").click(function() {
+        if($(this).is(":visible")){
+            $(this).css("display", "none");
+        }
+	});
+
+    $(".firewood").click(function() {
+        if($(this).is(":visible")){
+            $(this).css("display", "none");
+        }
+	});
+
+    $(".saw").click(function() {
+        if($(this).is(":visible")){
+            $(this).css("display", "none");
+        }
+	});
+
+});
